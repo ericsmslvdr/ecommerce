a548f7d1e4402df7e5b2c1bf07ac4afcc1985ac1
Eric Samuel Salvador <ericsmsalvador@gmail.com>
2023-12-17 15:55:32 +0800

    initial commit

---
+++ src/hooks/useToast.jsx
@@ -0,0 +1,18 @@
+const useToast = () => {
+    const triggerToast = (message) => {
+        toast.success(message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
+
+    return { triggerToast }
+}
+
+export default useToast
